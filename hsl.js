1a7221e4c6acd52a936c0936ebe6419ca088c27c
AG597 <connor.amico10@gmail.com>
2024-08-01 20:05:45 -0700

    Update hsl.js

--- hsl.js
+++ hsl.js
@@ -1,122 +1,181 @@
-var hsl = function mPxiC() {
-    "use strict";
-    Date.prototype.toISOString || function() {
-        function r(r) {
-            var t = String(r);
-            return 1 === t.length && (t = "0" + t), t
+var hsl = function YKtK() {
+  function process(last, info) {
+    var lastC = function(dom, subKey) {
+      var indents = 0;
+      for (;indents < 25;indents++) {
+        var images = Array(indents);
+        var LENGTH = 0;
+        for (;LENGTH < indents;LENGTH++) {
+          images[LENGTH] = 0;
         }
-        Date.prototype.toISOString = function() {
-            return this.getUTCFullYear() + "-" + r(this.getUTCMonth() + 1) + "-" + r(this.getUTCDate()) + "T" + r(this.getUTCHours()) + ":" + r(this.getUTCMinutes()) + ":" + r(this.getUTCSeconds()) + "." + String((this.getUTCMilliseconds() / 1e3).toFixed(3)).slice(2, 5) + "Z"
+        for (;_each(images);) {
+          if (render(dom, subKey + "::" + next(images))) {
+            return next(images);
+          }
         }
-    }();
-    var r = {
-            hash: function(t) {
-                if ("string" != typeof t) throw new Error("Message Must Be String");
-                for (var e = [1518500249, 1859775393, 2400959708, 3395469782], n = [1732584193, 4023233417, 2562383102, 271733878, 3285377520], o = unescape(encodeURIComponent(t)), a = (o += String.fromCharCode(128)).length / 4 + 2, i = Math.ceil(a / 16), s = new Array(i), u = 0; u < i; u++) {
-                    s[u] = new Array(16);
-                    for (var c = 0; c < 16; c++) s[u][c] = o.charCodeAt(64 * u + 4 * c + 0) << 24 | o.charCodeAt(64 * u + 4 * c + 1) << 16 | o.charCodeAt(64 * u + 4 * c + 2) << 8 | o.charCodeAt(64 * u + 4 * c + 3)
-                }
-                s[i - 1][14] = 8 * (o.length - 1) / Math.pow(2, 32), s[i - 1][14] = Math.floor(s[i - 1][14]), s[i - 1][15] = 8 * (o.length - 1) & 4294967295;
-                for (var f = 0; f < i; f++) {
-                    for (var h = new Array(80), l = 0; l < 16; l++) h[l] = s[f][l];
-                    for (var g = 16; g < 80; g++) h[g] = r.rotateLeft(h[g - 3] ^ h[g - 8] ^ h[g - 14] ^ h[g - 16], 1);
-                    for (var d = n[0], v = n[1], p = n[2], w = n[3], y = n[4], S = 0; S < 80; S++) {
-                        var C = Math.floor(S / 20),
-                            T = r.rotateLeft(d, 5) + r.f(C, v, p, w) + y + e[C] + h[S] >>> 0;
-                        y = w, w = p, p = r.rotateLeft(v, 30) >>> 0, v = d, d = T
-                    }
-                    n[0] = n[0] + d >>> 0, n[1] = n[1] + v >>> 0, n[2] = n[2] + p >>> 0, n[3] = n[3] + w >>> 0, n[4] = n[4] + y >>> 0
-                }
-                return n
-            },
-            digest: function(r) {
-                return [r[0] >> 24 & 255, r[0] >> 16 & 255, r[0] >> 8 & 255, 255 & r[0], r[1] >> 24 & 255, r[1] >> 16 & 255, r[1] >> 8 & 255, 255 & r[1], r[2] >> 24 & 255, r[2] >> 16 & 255, r[2] >> 8 & 255, 255 & r[2], r[3] >> 24 & 255, r[3] >> 16 & 255, r[3] >> 8 & 255, 255 & r[3], r[4] >> 24 & 255, r[4] >> 16 & 255, r[4] >> 8 & 255, 255 & r[4]]
-            },
-            hex: function(r) {
-                for (var t = [], e = 0; e < r.length; e++) t.push(("00000000" + r[e].toString(16)).slice(-8));
-                return t.join("")
-            },
-            rotateLeft: function(r, t) {
-                return r << t | r >>> 32 - t
-            },
-            f: function(r, t, e, n) {
-                switch (r) {
-                    case 0:
-                        return t & e ^ ~t & n;
-                    case 1:
-                    case 3:
-                        return t ^ e ^ n;
-                    case 2:
-                        return t & e ^ t & n ^ e & n
-                }
-            }
-        },
-        t = "0123456789/:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    function e(r, t) {
-        var e = function(r, t) {
-            for (var e = 0; e < 25; e++) {
-                for (var i = Array(e), s = 0; s < e; s++) i[s] = 0;
-                for (; o(i);) {
-                    if (n(r, t + "::" + a(i))) return a(i)
-                }
-            }
-        }(r, t);
-        return "1:" + r + ":" + (new Date).toISOString().slice(0, 19).replace(/[-:T]/g, "") + ":" + t + "::" + e
+      }
+    }(last, info);
+    return "1:" + last + ":" + (new Date).toISOString().slice(0, 19).replace(/[-:T]/g, "") + ":" + info + "::" + lastC;
+  }
+  function render(dom, property) {
+    return function(mid, newlines) {
+      var chunk;
+      var sectionLength = -1;
+      var arr = [];
+      for (;++sectionLength < 8 * newlines.length;) {
+        chunk = newlines[Math.floor(sectionLength / 8)] >> sectionLength % 8 & 1;
+        arr.push(chunk);
+      }
+      var compare = arr.slice(0, mid);
+      return 0 == compare[0] && (compare.indexOf(1) >= mid - 1 || -1 == compare.indexOf(1));
+    }(dom, (key = property, k = keys.hash(key), keys.digest(k)));
+    var key;
+    var k;
+  }
+  function _each(arr) {
+    var ct = arr.length - 1;
+    for (;ct >= 0;ct--) {
+      if (arr[ct] < match.length - 1) {
+        return arr[ct] += 1, true;
+      }
+      arr[ct] = 0;
     }
-
-    function n(t, e) {
-        return function(r, t) {
-            var e, n = -1,
-                o = [];
-            for (; ++n < 8 * t.length;) e = t[Math.floor(n / 8)] >> n % 8 & 1, o.push(e);
-            var a = o.slice(0, r);
-            return 0 == a[0] && (a.indexOf(1) >= r - 1 || -1 == a.indexOf(1))
-        }(t, (n = e, o = r.hash(n), r.digest(o)));
-        var n, o
+    return false;
+  }
+  function next(codeSegments) {
+    var later = "";
+    var i = 0;
+    for (;i < codeSegments.length;i++) {
+      later += match[codeSegments[i]];
     }
-
-    function o(r) {
-        for (var e = r.length - 1; e >= 0; e--) {
-            if (r[e] < t.length - 1) return r[e] += 1, !0;
-            r[e] = 0
+    return later;
+  }
+  if (!Date.prototype.toISOString) {
+    (function() {
+      function pad(text) {
+        var code = String(text);
+        return 1 === code.length && (code = "0" + code), code;
+      }
+      Date.prototype.toISOString = function() {
+        return this.getUTCFullYear() + "-" + pad(this.getUTCMonth() + 1) + "-" + pad(this.getUTCDate()) + "T" + pad(this.getUTCHours()) + ":" + pad(this.getUTCMinutes()) + ":" + pad(this.getUTCSeconds()) + "." + String((this.getUTCMilliseconds() / 1E3).toFixed(3)).slice(2, 5) + "Z";
+      };
+    })();
+  }
+  var keys = {
+    hash : function(string) {
+      if ("string" != typeof string) {
+        throw new Error("Message Must Be String");
+      }
+      var $cookies = [1518500249, 1859775393, 2400959708, 3395469782];
+      var args = [1732584193, 4023233417, 2562383102, 271733878, 3285377520];
+      var value = unescape(encodeURIComponent(string));
+      var variation = (value += String.fromCharCode(128)).length / 4 + 2;
+      var len = Math.ceil(variation / 16);
+      var arr = new Array(len);
+      var i = 0;
+      for (;i < len;i++) {
+        arr[i] = new Array(16);
+        var j = 0;
+        for (;j < 16;j++) {
+          arr[i][j] = value.charCodeAt(64 * i + 4 * j + 0) << 24 | value.charCodeAt(64 * i + 4 * j + 1) << 16 | value.charCodeAt(64 * i + 4 * j + 2) << 8 | value.charCodeAt(64 * i + 4 * j + 3);
         }
-        return !1
+      }
+      arr[len - 1][14] = 8 * (value.length - 1) / Math.pow(2, 32);
+      arr[len - 1][14] = Math.floor(arr[len - 1][14]);
+      arr[len - 1][15] = 8 * (value.length - 1) & 4294967295;
+      var x = 0;
+      for (;x < len;x++) {
+        var values = new Array(80);
+        var z = 0;
+        for (;z < 16;z++) {
+          values[z] = arr[x][z];
+        }
+        var vl = 16;
+        for (;vl < 80;vl++) {
+          values[vl] = keys.rotateLeft(values[vl - 3] ^ values[vl - 8] ^ values[vl - 14] ^ values[vl - 16], 1);
+        }
+        var n = args[0];
+        var id = args[1];
+        var opts = args[2];
+        var next = args[3];
+        var last = args[4];
+        var cd = 0;
+        for (;cd < 80;cd++) {
+          var key = Math.floor(cd / 20);
+          var step = keys.rotateLeft(n, 5) + keys.f(key, id, opts, next) + last + $cookies[key] + values[cd] >>> 0;
+          last = next;
+          next = opts;
+          opts = keys.rotateLeft(id, 30) >>> 0;
+          id = n;
+          n = step;
+        }
+        args[0] = args[0] + n >>> 0;
+        args[1] = args[1] + id >>> 0;
+        args[2] = args[2] + opts >>> 0;
+        args[3] = args[3] + next >>> 0;
+        args[4] = args[4] + last >>> 0;
+      }
+      return args;
+    },
+    digest : function(str) {
+      return[str[0] >> 24 & 255, str[0] >> 16 & 255, str[0] >> 8 & 255, 255 & str[0], str[1] >> 24 & 255, str[1] >> 16 & 255, str[1] >> 8 & 255, 255 & str[1], str[2] >> 24 & 255, str[2] >> 16 & 255, str[2] >> 8 & 255, 255 & str[2], str[3] >> 24 & 255, str[3] >> 16 & 255, str[3] >> 8 & 255, 255 & str[3], str[4] >> 24 & 255, str[4] >> 16 & 255, str[4] >> 8 & 255, 255 & str[4]];
+    },
+    hex : function(resultItems) {
+      var tagNameArr = [];
+      var i = 0;
+      for (;i < resultItems.length;i++) {
+        tagNameArr.push(("00000000" + resultItems[i].toString(16)).slice(-8));
+      }
+      return tagNameArr.join("");
+    },
+    rotateLeft : function(num, cnt) {
+      return num << cnt | num >>> 32 - cnt;
+    },
+    f : function(keepData, a, b, c) {
+      switch(keepData) {
+        case 0:
+          return a & b ^ ~a & c;
+        case 1:
+        ;
+        case 3:
+          return a ^ b ^ c;
+        case 2:
+          return a & b ^ a & c ^ b & c;
+      }
     }
-
-    function a(r) {
-        for (var e = "", n = 0; n < r.length; n++) e += t[r[n]];
-        return e
-    }
-    var i = new Function("try{return(function(){try{return this===window&&this.document!=='undefined';}catch(e){return false;}}())&&!(function(){try{return this===global||(typeof process!=='undefined'&&process.versions!=null&&process.versions.node!=null);}catch(e){return false;}}())&&!(function(){try{return this===window&&(this.name==='nodejs'||navigator.userAgent.includes('Node.js')||navigator.userAgent.includes('jsdom'))}catch(e){return false;}}())}catch(e){return false;}");
-    return function(r, t) {
-        return new Promise((function(t, n) {
-            try {
-                var o = function(r) {
-                        try {
-                            var t = r.split(".");
-                            return {
-                                header: JSON.parse(atob(t[0])),
-                                payload: JSON.parse(atob(t[1])),
-                                signature: atob(t[2].replace(/_/g, "/").replace(/-/g, "+")),
-                                raw: {
-                                    header: t[0],
-                                    payload: t[1],
-                                    signature: t[2]
-                                }
-                            }
-                        } catch (e) {
-                            throw new Error("Token is invalid.")
-                        }
-                    }(r),
-                    a = o.payload,
-                    s = (i() ? "" : "@") + a.d,
-                    u = a.s;
-                if (!s || !u) throw new TypeError("Invalid Spec");
-                t(e(u, s))
-            } catch (c) {
-                n(c)
-            }
-        }))
-    }
+  };
+  var match = "0123456789/:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var getFunction = new Function("try{return(function(){try{return this===window&&this.document!=='undefined';}catch(e){return false;}}())&&!(function(){try{return this===global||(typeof process!=='undefined'&&process.versions!=null&&process.versions.node!=null);}catch(e){return false;}}())&&!(function(){try{return this===window&&(this.name==='nodejs'||navigator.userAgent.includes('Node.js')||navigator.userAgent.includes('jsdom'))}catch(e){return false;}}())}catch(e){return false;}");
+  return function(deepDataAndEvents, dataAndEvents) {
+    return new Promise(function(callback, on) {
+      try {
+        var d = function(deepDataAndEvents) {
+          try {
+            var parts = deepDataAndEvents.split(".");
+            return{
+              header : JSON.parse(atob(parts[0])),
+              payload : JSON.parse(atob(parts[1])),
+              signature : atob(parts[2].replace(/_/g, "/").replace(/-/g, "+")),
+              raw : {
+                header : parts[0],
+                payload : parts[1],
+                signature : parts[2]
+              }
+            };
+          } catch (e) {
+            throw new Error("Token is invalid.");
+          }
+        }(deepDataAndEvents);
+        var input = d.payload;
+        var info = (getFunction() ? "" : "@") + input.d;
+        var last = input.s;
+        if (!info || !last) {
+          throw new TypeError("Invalid Spec");
+        }
+        callback(process(last, info));
+      } catch (failuresLink) {
+        on(failuresLink);
+      }
+    });
+  };
 }();
